f70bba41da1a56f92c703209d284d7dddb70e499
asher9889 <saurabhkushwaha9889@gmail.com>
2024-08-15 21:52:45 +0530

    gpt input box completed

--- src/Pages/GPT.js
+++ src/Pages/GPT.js
@@ -1,14 +1,16 @@
 import BrowserHeader from "../components/BrowserHeader";
+import GPTInputBox from "../components/GPTInputBox";
+import GPTResultBox from "../components/GPTResultBox";
 
-const GPT = ()=>{
-    return (
-        <div className=" text-xl  bg-signInBg">
-            <BrowserHeader />
-            <div>
-                
-            </div>
-        </div>
-    )
-}
+const GPT = () => {
+  return (
+    <div className="w-full text-xl  bg-signInBg  ">
+      <BrowserHeader />
+      <GPTInputBox />
+      <GPTResultBox />
+      
+    </div>
+  );
+};
 
 export default GPT;
---
+++ src/components/GPTResultBox.js
@@ -0,0 +1,9 @@
+const GPTResultBox = ()=>{
+    return (
+        <div className="bg-red-500 mt-[5%]">
+            GsultBoxPTRe
+        </div>
+    )
+}
+
+export default GPTResultBox;
